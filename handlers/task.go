8b85611725da05bab497a91bb256da8e3c5f4f7e
osmankantarcioglu <126028466+osmankantarcioglu@users.noreply.github.com>
2025-06-08 14:01:28 +0200

    Entegration of UPDATE task API

--- handlers/task.go
+++ handlers/task.go
@@ -49,3 +49,47 @@ func GetTasks(c *fiber.Ctx) error {
 
 	return c.JSON(tasks)
 }
+
+func UpdateTask(c *fiber.Ctx) error {
+	userID := c.Locals("userID").(uint)
+	taskID := c.Params("id")
+
+	var task models.Task
+	if err := database.DB.First(&task, "id = ? AND user_id = ?", taskID, userID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	type UpdateInput struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+		Done        *bool  `json:"done"`
+	}
+
+	var input UpdateInput
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// Sadece gelen alanları güncelle
+	if input.Title != "" {
+		task.Title = input.Title
+	}
+	if input.Description != "" {
+		task.Description = input.Description
+	}
+	if input.Done != nil {
+		task.Done = *input.Done
+	}
+
+	if err := database.DB.Save(&task).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(task)
+}
--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	app.Post("/login", handlers.Login)
 	app.Post("/tasks", middleware.Protected(), handlers.CreateTask)
 	app.Get("/tasks", middleware.Protected(), handlers.GetTasks)
+	app.Put("/tasks/:id", middleware.Protected(), handlers.UpdateTask)
 
 	app.Listen(":3000")
 }
